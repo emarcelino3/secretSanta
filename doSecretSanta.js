8086e497d3072b37a24495ce440d7b06c2476254
Eduardo <eduardo.marcelino3@gmail.com>
2020-12-17 18:42:10 -0300

    'corrections'

--- doSecretSanta.js
+++ doSecretSanta.js
@@ -7,6 +7,17 @@ function doSecretSanta(list) {
     .sort(() => Math.random() > .5 ? 1 : -1 )
     .map( (item, idx, arr) =>  [item, arr[ idx === arr.length-1 ? 0 : idx + 1 ]] )
 }
+
+function* doSecretSantaYield([...list]) {
+    list.sort( _ => Math.random() > .5 ? 1 : -1 )
+    let first = list[0]
+    while(list.length)
+    yield [list.shift(), list[0] || first ]
+  }
   
+//   console.table( [...doSecretSanta(friends)] )
+
+
 module.exports.doSecretSanta = doSecretSanta
+module.exports.doSecretSantaYield = doSecretSantaYield
 
--- tests/index.test.js
+++ tests/index.test.js
@@ -1,7 +1,8 @@
 const assert = require('assert').strict;
 const {doSecretSanta} = require('../doSecretSanta')
+const {doSecretSantaYield} = require('../doSecretSanta')
 
-describe('Unit Tests',()=>{
+describe('Unit Tests - doSecretSanta',()=>{
     it('List contem items', ()=>{        
         const friends = ['joão', 'Maria', 'José', 'Carlos', 'Ana', 'Julia', 'Denis']
         assert.notStrictEqual(doSecretSanta(friends).length, 6)        
@@ -9,6 +10,18 @@ describe('Unit Tests',()=>{
     it('Shuffle array', ()=>{        
         const friends = ['joão', 'Maria', 'José', 'Carlos', 'Ana', 'Julia', 'Denis']
         const arr = doSecretSanta(friends).map(item=> item[0]  )
+        assert.notDeepStrictEqual(arr, friends)              
+    })
+})
+
+describe('Unit Tests - doSecretSantaYield',()=>{
+    it('List contem items', ()=>{        
+        const friends = ['joão', 'Maria', 'José', 'Carlos', 'Ana', 'Julia', 'Denis']
+        assert.notStrictEqual([...doSecretSantaYield(friends)].length, 6)        
+    })
+    it('Shuffle array', ()=>{        
+        const friends = ['joão', 'Maria', 'José', 'Carlos', 'Ana', 'Julia', 'Denis']
+        const arr = [...doSecretSantaYield(friends)].map(item=> item[0]  )
         assert.notDeepStrictEqual(arr, friends)              
     })
 })
